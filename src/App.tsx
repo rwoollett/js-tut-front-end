4470cd7583fc25d6333af9931ffaf202922f1a1e
Rodney Woollett <rodney.woollett@gmail.com>
2021-02-07 21:30:13 +1300

    First React Component and FC - prepared for mockup of SaaS style web page - as in YouTube

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,45 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 
-const App = ({title}: {title: string}) => <div>Hello World {title}</div>;
+interface AppProps {
+  title: string;
+  done?: boolean;
+}
+
+class App extends React.Component<AppProps, unknown> {
+  static defaultProps: Partial<AppProps> = {
+    done: false,
+  }
+
+  constructor(props: AppProps) {
+    super(props);
+  }
+
+  render() {
+    const {title, done} = this.props;
+    return (<div>
+             <div>Hello World {title} {done}</div>
+              <TodoItem text="update 2"/>
+              <ReactFCComponent text="my component">
+                <p>one</p>
+                <p>two</p>
+              </ReactFCComponent> 
+            </div>);
+  }
+} 
+
+interface TodoItemProps {
+  done?: boolean,
+  text: string,
+}
+
+const TodoItem = ({done = false, text}: TodoItemProps) => {
+   return <div>{text} {done}</div>;
+};
+
+const ReactFCComponent: React.FC<{text:string}> = ({children, text}) => {
+  return <div><h1>{text}</h1>{children}</div>;
+};
+
 
 export default hot(App);
